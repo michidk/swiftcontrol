ffa604f9215eaa2a8e3966b6052aba0646eef4b6
Jonas Bark <jonas.bark@gmx.de>
2025-09-08 12:59:04 +0200

    fix logging messages

--- lib/utils/actions/android.dart
+++ lib/utils/actions/android.dart
@@ -43,8 +43,8 @@ class AndroidActions extends BaseActions {
     final point = supportedApp!.resolveTouchPosition(action: button, windowInfo: windowInfo);
     if (point != Offset.zero) {
       accessibilityHandler.performTouch(point.dx, point.dy);
-      return "No touch performed";
+      return "Touch performed at: ${point.dx.toInt()}, ${point.dy.toInt()}";
     }
-    return "Touch performed at: ${point.dx.toInt()}, ${point.dy.toInt()}";
+    return "No touch performed";
   }
 }
