13c70fc445dfbdf1b4b094db4fab130644481f34
Jonas Bark <jonas.bark@gmx.de>
2025-09-22 09:28:35 +0200

    enable encryption for Zwift Click v2 to potentially fix #68

--- lib/bluetooth/devices/zwift_clickv2.dart
+++ lib/bluetooth/devices/zwift_clickv2.dart
@@ -2,4 +2,7 @@ import 'package:swift_control/bluetooth/devices/zwift_ride.dart';
 
 class ZwiftClickV2 extends ZwiftRide {
   ZwiftClickV2(super.scanResult);
+
+  @override
+  bool get supportsEncryption => true;
 }
